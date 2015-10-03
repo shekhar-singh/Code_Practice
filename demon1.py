0f245dc09c1967de0f7a4eb7b7683802049904ab
shekhar-singh <sschoudhary60@gmail.com>
2015-10-03 22:07:41 +0530

    Deamon process

---
+++ demon1.py
@@ -0,0 +1,16 @@
+#!usr/bin env python
+import daemon
+import time
+
+def action():
+    while True:
+    	with open("/tmp/readme.txt", "w") as f:
+        	f.write("your program is running silently and this file is create at  "+time.ctime())
+        time.sleep(10)
+
+def run():
+    with daemon.DaemonContext():
+        action()
+
+if __name__ == "__main__":
+    run()
