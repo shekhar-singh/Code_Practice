9a6d0b5e2a26619a33197e405214f2a5ad402cfd
shekhar-singh <sschoudhary60@gmail.com>
2016-02-12 12:31:20 +0530

    tast3 in nodejs

---
+++ nodejs/task3.js
@@ -0,0 +1,27 @@
+var Task = function(name){
+	this.name=name;
+	this.completed=false;
+	
+}
+
+Task.prototype.complete=function () {
+	console.log('completing task:'+this.name);
+        this.completed=true;
+};
+
+Task.prototype.save=function  () {
+	
+		console.log('saving Task:'+this.name);
+};
+
+
+module.exports=Task;
+// var task1=new Task('create a demo for constructor');
+// var task2=new Task('create a demo for module');
+// var task3=new Task('create a demo for singletons');
+// var task4=new Task('create a demo for prototype');
+
+// task1.complete();
+// task2.save();
+// task3.save();
+// task4.save();
